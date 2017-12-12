10a730b2a4271945c83bef09f8d609b86a79f09b
Technifutur <600@technifutur.be>
2017-12-12 12:02:03 +0100

    envoi sur giyhub

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : DualBehaviour 
+{
+    #region Public Members
+
+    #endregion
+
+    #region Public Void
+
+    #endregion
+
+    #region System
+
+    void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+       
+	}
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+   
+    #endregion
+}
---
+++ Assets/Script/reigns.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class reigns : DualBehaviour 
+{
+    #region Public Members
+    public Image avatarImage;
+    public Image [] avatars;
+    public Image _avatarImage;
+    public int _number;
+    public string _nomImg;
+    public Button _yesButton;
+    public Button _noButton;
+    public int _conteurYes;
+    public int _conteurNo;
+    #endregion
+
+    #region Public Void
+
+    #endregion
+
+    #region System
+    private void Awake()
+    {
+        _yesButton.onClick.AddListener(YesClick);
+        _noButton.onClick.AddListener(NoClick);
+    }
+
+
+    void Start () 
+    {
+        _avatarImage.sprite = avatars[2];
+      
+
+       
+
+    }
+
+    void Update () 
+    {
+       
+	}
+   
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+   private void YesClick ()
+    {
+
+        Debug.Log("you cliked yes" + _conteurYes);
+        changePic();
+      
+
+    }
+
+    private void NoClick()
+    {
+        Debug.Log("you cliked no" + _conteurNo);
+        changePic();
+
+    }
+    public void  changePic()
+    {
+        _number = Random.Range(2, 9);
+        // _nomImg = _number.ToString() + ".png";
+        _avatarImage.sprite = avatars[_number];
+
+    }
+    #endregion
+}
